b00df99c3dac2c04f03911a3f02103c05f8bef01
AstralNetwork <78843614+AstralNetwork@users.noreply.github.com>
2021-03-24 12:32:42 +0800

    Create examples.js

---
+++ examples.js
@@ -0,0 +1,13 @@
+const rxl = require('rxl.js')
+
+console.log(rxl.getUserdataFromId(1)); // See? Pretty simple isn't it
+
+console.log(rxl.getUserGroupsFromId(1)); //Get's the user's group from the userid
+
+console.log(rxl.getUserdataFromUsername("AstralNetwork")); //Get's the user's data such as their ID, Username, Presence, etc
+
+console.log(rxl.getIdFromUsername("AstralNetwork")); //Returns the user's ID
+
+console.log(rxl.getUsernameFromId(78421035)); //Returns the user's username from ID
+
+//You can do it, I believe in you\\
